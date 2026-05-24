8c0593b7c1a6104ecdb23bcf187f8cb921572df1
atsmile <atsmile8723@gmail.com>
2026-05-25 07:29:43 +0900

    feat: BlogIntroコンポーネント追加

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { getList } from '@/libs/microcms';
 import { LIMIT } from '@/constants';
 import Pagination from '@/components/Pagination';
 import ArticleList from '@/components/ArticleList';
+import BlogIntro from '@/components/BlogIntro';
 
 export default async function Page() {
   const data = await getList({
@@ -9,6 +10,7 @@ export default async function Page() {
   });
   return (
     <>
+      <BlogIntro />
       <ArticleList articles={data.contents} />
       <Pagination totalCount={data.totalCount} />
     </>
